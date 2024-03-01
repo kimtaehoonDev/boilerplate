bc4962fe9cbc7284ad6b0e6156c44d1e9f30458a
kimtaehoonDev <thkimm.dev@gmail.com>
2024-03-02 00:55:41 +0900

    [#1] chore: application.yml 프로필 분리

--- src/main/java/kimtaehoon/boilerplate/BoilerplateApplication.java
+++ src/main/java/kimtaehoon/boilerplate/BoilerplateApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BoilerplateApplication {
 
+	// TODO 1. 프로그램이 실행되는지 확인합니다.
+	// TODO 2. gitignore 파일 인코딩을 UTF-8로 변경한 뒤, 필요한 정보들을 추가합니다.
+	// TODO 3. application.properties 파일을 삭제하고, application.yml 파일을 생성합니다.
+	// TODO 4. application.yml 의 인코딩을 UTF-8로 변경합니다.
+	// TODO 5. application.yml 내부에서 프로필을 분리합니다. (Optional)
 	public static void main(String[] args) {
 		SpringApplication.run(BoilerplateApplication.class, args);
 	}
