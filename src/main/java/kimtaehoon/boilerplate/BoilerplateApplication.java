e5c6aa76643dccac47a977398997a756279cc834
kimtaehoonDev <thkimm.dev@gmail.com>
2024-03-02 00:59:35 +0900

    [#1] style: 깃허브 설정 가이드라인 추가

--- src/main/java/kimtaehoon/boilerplate/BoilerplateApplication.java
+++ src/main/java/kimtaehoon/boilerplate/BoilerplateApplication.java
@@ -11,6 +11,9 @@ public class BoilerplateApplication {
 	// TODO 3. application.properties 파일을 삭제하고, application.yml 파일을 생성합니다.
 	// TODO 4. application.yml 의 인코딩을 UTF-8로 변경합니다.
 	// TODO 5. application.yml 내부에서 프로필을 분리합니다. (Optional)
+
+	// TODO 8. 깃허브에서 특정 브랜치(ex. main)로 직접 푸쉬하지 못하도록 설정합니다.
+	// TODO 9. 깃허브에서 라벨들을 커스터마이징합니다.
 	public static void main(String[] args) {
 		SpringApplication.run(BoilerplateApplication.class, args);
 	}
