c69090fb59c9367bd9f0871e12070da81be40427
kimtaehoonDev <thkimm.dev@gmail.com>
2024-03-02 00:59:55 +0900

    [#1] style: 전체 주석 삭제

--- src/main/java/kimtaehoon/boilerplate/BoilerplateApplication.java
+++ src/main/java/kimtaehoon/boilerplate/BoilerplateApplication.java
@@ -6,14 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BoilerplateApplication {
 
-	// TODO 1. 프로그램이 실행되는지 확인합니다.
-	// TODO 2. gitignore 파일 인코딩을 UTF-8로 변경한 뒤, 필요한 정보들을 추가합니다.
-	// TODO 3. application.properties 파일을 삭제하고, application.yml 파일을 생성합니다.
-	// TODO 4. application.yml 의 인코딩을 UTF-8로 변경합니다.
-	// TODO 5. application.yml 내부에서 프로필을 분리합니다. (Optional)
-
-	// TODO 8. 깃허브에서 특정 브랜치(ex. main)로 직접 푸쉬하지 못하도록 설정합니다.
-	// TODO 9. 깃허브에서 라벨들을 커스터마이징합니다.
 	public static void main(String[] args) {
 		SpringApplication.run(BoilerplateApplication.class, args);
 	}
