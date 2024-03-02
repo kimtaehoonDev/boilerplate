daa6433e1308d93c46aa26e09d6934c1e8386b20
kimtaehoonDev <thkimm.dev@gmail.com>
2024-03-02 13:26:48 +0900

    [#8] style: 전체 주석 삭제

--- src/main/java/kimtaehoon/boilerplate/config/WebConfig.java
+++ src/main/java/kimtaehoon/boilerplate/config/WebConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// TODO 1. WebConfig에 addCorsMappings 설정을 추가한다.
-//  주의! 만약 이후에 Interceptor를 등록한다면, OPTIONS 메서드는 통과할 수 있게 만들어줘야 한다.
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
